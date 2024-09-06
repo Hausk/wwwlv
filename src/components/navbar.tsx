3a0b758534de750f18af01630fdd0bc97c9b5c69
Hausk <dbessa.jonathan@gmail.com>
2024-09-06 14:50:14 +0200

    update Links Photobox.

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,33 +1,36 @@
-'use client'
-import Link from "next/link"
-import { usePathname } from "next/navigation"
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { Button } from "./ui/button";
 
 const links = [
-    {
-        name: 'accueil',
-        path: '/'
-    },
-    {
-        name: 'photobox',
-        path: '/works'
-    },
+  {
+    name: "accueil",
+    path: "/",
+  },
+  {
+    name: "photobox",
+    path: "/photobox",
+  },
 ];
 
 export const Navbar = () => {
-    const pathName = usePathname()
-    return (
-        <nav className="flex gap-8">
-            {links.map((link, index) => {
-                return (
-                    <Link href={link.path} key={index} className={`${
-                        link.path === pathName && "text-accent border-b-2 border-accent"
-                        } capitalize font-medium hover:text-accent transition-all`}
-                    >
-                        {link.name}
-                    </Link>
-                )
-            })}
-        </nav>
-    )
-}
+  const pathName = usePathname();
+  return (
+    <nav className="flex gap-8">
+      {links.map((link, index) => {
+        return (
+          <Link
+            href={link.path}
+            key={index}
+            className={`${
+              link.path === pathName && "text-accent border-b-2 border-accent"
+            } capitalize font-medium hover:text-accent transition-all`}
+          >
+            {link.name}
+          </Link>
+        );
+      })}
+    </nav>
+  );
+};
--- src/components/works-card.tsx
+++ src/components/works-card.tsx
@@ -1,32 +1,37 @@
 "use client";
- 
+
 import Image from "next/image";
 import React from "react";
 import { CardContainer, CardItem } from "@/components/3d-card";
 import Link from "next/link";
- 
-export function WorksCard({work}:{work: any}) {
+
+export function WorksCard({ work }: { work: any }) {
   return (
     <CardContainer className="relative md:w-[60vh] w-4/6 lg:w-4/6">
-        <Link 
-            className="w-full max-h-[50%] relative"
-            href={'/work/' + work.slug}
+      <Link
+        className="w-full max-h-[50%] relative"
+        href={"/photobox/" + work.slug}
+      >
+        <CardItem
+          translateZ="100"
+          className="flex overflow-hidden w-full h-fit max-h-[75vh] max-w-[75vh] md:h-[60vh] lg:h-fit rounded-md mt-4 relative"
         >
-        <CardItem translateZ="100" className="flex overflow-hidden w-full h-fit max-h-[75vh] max-w-[75vh] md:h-[60vh] lg:h-fit rounded-md mt-4 relative">
           <Image
-            src={work.src ?? ''}
+            src={work.src ?? ""}
             height={2000}
             width={2000}
             className="h-auto w-full object-cover aspect-square rounded-xl group-hover/card:shadow-xl m-auto"
             alt="thumbnail"
-        />
+          />
         </CardItem>
         <CardItem
-            translateZ={20}
-            className="w-fit text-4xl font-semibold m-auto pt-2" as="p">
-            {work.title}
+          translateZ={20}
+          className="w-fit text-4xl font-semibold m-auto pt-2"
+          as="p"
+        >
+          {work.title}
         </CardItem>
-        </Link>
+      </Link>
     </CardContainer>
   );
 }
