c445c2782bf399c8ad646797dcbe21271a1a9950
Hausk <dbessa.jonathan@gmail.com>
2024-09-06 14:44:25 +0200

    Update du routing name.

--- src/components/photo.tsx
+++ src/components/photo.tsx
@@ -38,10 +38,10 @@ export const Photo = () => {
             </motion.div>
         </motion.div>
         <motion.svg className="w-[70vw] h-[70vw] xl:w-[506px] xl:h-[506px] absolute top-0" fill="transparent" viewBox="0 0 506 506" xmlns="https://www.w3.org/2000/svg">
-            <motion.circle cx="253" cy="253" r="250" stroke="#Ef4444" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round"
+            <motion.circle cx="253" cy="253" r="250" stroke="#Ef4444" strokeWidth="5" strokeLinecap="round" strokeLinejoin="round"
                 initial={{strokeDasharray: "24 10 0 0"}}
                 animate={{
-                    strokeDasharray: ["15 120 25 25", "16 25 92 72", "4 250 22 22", "4 25 22 22"],
+                    strokeDasharray: ["15 120 25 25", "16 25 92 72", "4 250 22 22"],
                     rotate: [120, 360],
                 }}
                 transition={{
